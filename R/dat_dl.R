3c4dde93eeb80e64f2d5e09c8eef46b18971daf4
jgao <jacorygao@outlook.com>
2022-03-29 23:09:30 -0400

    Redefine some API

--- R/dat_dl.R
+++ R/dat_dl.R
@@ -2,3 +2,56 @@
 # Description: Download Landsat time series using AppEEARS.
 # Date: 2022-03-27
 #************************************************************************************
+
+
+
+#' Request Landsat ARD point time series data using AppEEARS.
+#' 
+#' @param pts Points of the time series.
+#' @param username,password Username and Password for login to AppEEARS.
+#' @return TRUE if the job is successfully submited.
+#' @export
+#' 
+#' @examples 
+#' /dontrun {
+#' 
+#' }
+ReqArdPoint <- function(pts, username, password) {
+    
+}
+
+
+#' Download the job from AppEEARS.
+#' 
+#' @param job_id Job ID.
+#' @param out_dir The target directory. 
+#' @return TRUE if done.
+#' @export
+#' 
+#' @examples 
+#' /dontrun {
+#' 
+#' }
+DlArdPoint <- function(job_id, out_dir) {
+
+}
+
+
+
+
+#' Check AppEEARS job status. 
+#' 
+#' @param regstr Filter string used to select interested jobs.
+#' @return 
+#' @export
+#' 
+#' @examples 
+#' /dontrun {
+#' 
+#' }
+JobStatus <- function(regstr) {
+
+}
+
+
+
