4af021f402cb240b0279bd707e2201c7563d4814
MrJGao <jacorygao@outlook.com>
2022-05-28 19:07:10 -0400

    Check `weights` to be in the range of [0, 1]

--- R/mod_fit.R
+++ R/mod_fit.R
@@ -38,6 +38,11 @@ FitBLSP <- function(date_vec, vi_vec,
         stop("There're invalid Date values in the `date_vec`! 
             Be sure to use `yyyy-mm-dd` format.")
     }
+
+    # Check weights to be in the range of [0, 1]
+    if (min(weights_vec) < 0 & max(weights_vec) > 1) {
+        stop("Weights must be within [0, 1].")
+    }
     
     # Convert data to jags format
     y <- vi_vec
