604ce39b0b3b830e30d1fccefc88e05a64b1cd20
MrJGao <jacorygao@outlook.com>
2022-06-02 17:31:18 -0400

    Document the example data

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Landsat EVI2 time series.
+#' 
+#' An Example EVI2 time series from Landsat observations. 
+#' 
+#' @format A data frame with 802 rows and 3 variables:
+#' \describe{
+#'   \item{date}{Observation dates}
+#'   \item{evi2}{The two-band enhanced vegetation index value}
+#'   \item{snow}{Logical, indicates whether the observation contains snow}
+#' }
+"landsatEVI2"
